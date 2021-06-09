522f32cb841b25cfe6c3b62e71a90df9bccb3902
Tran Tu <tranhoangtu12091994@gmail.com>
2021-06-09 09:25:26 +0700

    Add error modal

---
+++ src/UI/ErrorModal.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import Button from "./Button";
+import Card from "./Card";
+import styled from "./ErrorModal.module.css";
+
+const ErrorModal = (props) => {
+  return (
+    <div>
+      <div className={styled.backdrop} onClick={props.onConfirm} />
+      <Card className={styled.modal}>
+        <header className={styled.header}>
+          <h2>{props.title}</h2>
+        </header>
+        <div className={styled.content}>
+          <p>{props.message}</p>
+        </div>
+        <footer className={styled.actions}>
+          <Button onClick={props.onConfirm}>Okay</Button>
+        </footer>
+      </Card>
+    </div>
+  );
+};
+export default ErrorModal;
--- src/Users/AddUser.js
+++ src/Users/AddUser.js
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 import styled from "./AddUser.module.css";
 import UsersList from "./UsersList";
 
 const AddUser = (props) => {
   const [username, setUsername] = useState("");
   const [age, setAge] = useState("");
+  const [error, setError] = useState(null);
   const addUserHandler = (e) => {
     e.preventDefault();
     if (username.trim().length === 0 || age.trim().length === 0) {
+      setError({ title: "Error", message: "Please input username or age" });
       return;
     }
     if (+age < 1) {
+      setError({ title: "Error", message: "Please input valid age" });
       return;
     }
     props.onAddUser(username, age);
@@ -27,26 +31,39 @@ const AddUser = (props) => {
     setAge(e.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className={styled.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlfor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={usernameChangeHandler}
-        ></input>
-        <label htmlfor="age">Age</label>
-        <input
-          type="number"
-          id="age"
-          value={age}
-          onChange={ageChangeHandler}
-        ></input>
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={styled.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlfor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={usernameChangeHandler}
+          ></input>
+          <label htmlfor="age">Age</label>
+          <input
+            type="number"
+            id="age"
+            value={age}
+            onChange={ageChangeHandler}
+          ></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
